c4fc3c27b52a1ddd0af2313a5341086fd19fbf69
Anthony <anthonnhani@gmail.com>
2026-05-14 11:47:36 -0600

    Added color schemes and fonts to css file as well as skeleton of app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const indexRoute = require("./routes/index");
+const indexRouter = require("./routes/index");
+const path = require("node:path");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: false}));
+
+
+app.use("/", indexRouter);
+
+
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+})
 
 
 
